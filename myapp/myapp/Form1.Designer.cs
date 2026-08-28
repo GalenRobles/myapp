5616f81cc8aecf9bb1875d6fbedcdfad42f97e04
Galen Robles <greciapicazoxd@gmail.com>
2026-08-28 15:41:40 -0600

    calculadora rama

--- myapp/myapp/Form1.Designer.cs
+++ myapp/myapp/Form1.Designer.cs
@@ -28,46 +28,97 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.txtNombre = new System.Windows.Forms.Label();
-            this.txtbox = new System.Windows.Forms.TextBox();
-            this.BtnProceso = new System.Windows.Forms.Button();
+            this.BtnCalcular = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNumero1 = new System.Windows.Forms.TextBox();
+            this.txtNumero2 = new System.Windows.Forms.TextBox();
+            this.txtNumero3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnlimpiar = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // txtNombre
+            // BtnCalcular
             // 
-            this.txtNombre.AutoSize = true;
-            this.txtNombre.Location = new System.Drawing.Point(224, 44);
-            this.txtNombre.Name = "txtNombre";
-            this.txtNombre.Size = new System.Drawing.Size(79, 20);
-            this.txtNombre.TabIndex = 0;
-            this.txtNombre.Text = "NOMBRE";
-            this.txtNombre.Click += new System.EventHandler(this.label1_Click);
+            this.BtnCalcular.Location = new System.Drawing.Point(239, 36);
+            this.BtnCalcular.Name = "BtnCalcular";
+            this.BtnCalcular.Size = new System.Drawing.Size(97, 44);
+            this.BtnCalcular.TabIndex = 0;
+            this.BtnCalcular.Text = "Calcular";
+            this.BtnCalcular.UseVisualStyleBackColor = true;
+            this.BtnCalcular.Click += new System.EventHandler(this.BtnCalcular_Click);
             // 
-            // txtbox
+            // label1
             // 
-            this.txtbox.Location = new System.Drawing.Point(228, 100);
-            this.txtbox.Name = "txtbox";
-            this.txtbox.Size = new System.Drawing.Size(284, 26);
-            this.txtbox.TabIndex = 1;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Numero 1";
             // 
-            // BtnProceso
+            // label2
             // 
-            this.BtnProceso.Location = new System.Drawing.Point(228, 190);
-            this.BtnProceso.Name = "BtnProceso";
-            this.BtnProceso.Size = new System.Drawing.Size(284, 53);
-            this.BtnProceso.TabIndex = 2;
-            this.BtnProceso.Text = "Enviar";
-            this.BtnProceso.UseVisualStyleBackColor = true;
-            this.BtnProceso.Click += new System.EventHandler(this.BtnProceso_Click);
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(35, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Numero 2";
+            // 
+            // txtNumero1
+            // 
+            this.txtNumero1.Location = new System.Drawing.Point(39, 45);
+            this.txtNumero1.Name = "txtNumero1";
+            this.txtNumero1.Size = new System.Drawing.Size(138, 26);
+            this.txtNumero1.TabIndex = 3;
+            // 
+            // txtNumero2
+            // 
+            this.txtNumero2.Location = new System.Drawing.Point(39, 135);
+            this.txtNumero2.Name = "txtNumero2";
+            this.txtNumero2.Size = new System.Drawing.Size(138, 26);
+            this.txtNumero2.TabIndex = 4;
+            // 
+            // txtNumero3
+            // 
+            this.txtNumero3.Location = new System.Drawing.Point(39, 218);
+            this.txtNumero3.Name = "txtNumero3";
+            this.txtNumero3.Size = new System.Drawing.Size(138, 26);
+            this.txtNumero3.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(35, 186);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Resultado";
+            // 
+            // btnlimpiar
+            // 
+            this.btnlimpiar.Location = new System.Drawing.Point(239, 164);
+            this.btnlimpiar.Name = "btnlimpiar";
+            this.btnlimpiar.Size = new System.Drawing.Size(102, 46);
+            this.btnlimpiar.TabIndex = 7;
+            this.btnlimpiar.Text = "Limpiar";
+            this.btnlimpiar.UseVisualStyleBackColor = true;
+            this.btnlimpiar.Click += new System.EventHandler(this.btnlimpiar_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.BtnProceso);
-            this.Controls.Add(this.txtbox);
-            this.Controls.Add(this.txtNombre);
+            this.ClientSize = new System.Drawing.Size(540, 323);
+            this.Controls.Add(this.btnlimpiar);
+            this.Controls.Add(this.txtNumero3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtNumero2);
+            this.Controls.Add(this.txtNumero1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BtnCalcular);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -77,9 +128,14 @@
 
         #endregion
 
-        private System.Windows.Forms.Label txtNombre;
-        private System.Windows.Forms.TextBox txtbox;
-        private System.Windows.Forms.Button BtnProceso;
+        private System.Windows.Forms.Button BtnCalcular;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNumero1;
+        private System.Windows.Forms.TextBox txtNumero2;
+        private System.Windows.Forms.TextBox txtNumero3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnlimpiar;
     }
 }
 
--- myapp/myapp/Form1.cs
+++ myapp/myapp/Form1.cs
@@ -24,8 +24,25 @@ namespace myapp
 
         private void BtnProceso_Click(object sender, EventArgs e)
         {
-            string nombre=txtbox.Text;
-            MessageBox.Show(nombre);
+           // string nombre=txtbox.Text;
+            //MessageBox.Show(nombre);
+        }
+
+        private void BtnCalcular_Click(object sender, EventArgs e)
+        {
+            int a, b = 0;
+            a = int.Parse(txtNumero1.Text);
+            b = Convert.ToInt32(txtNumero2.Text);
+            MessageBox.Show("la suma es :" + (a + b).ToString());
+            txtNumero3.Text = (a + b).ToString();
+
+        }
+
+        private void btnlimpiar_Click(object sender, EventArgs e)
+        {
+           txtNumero1.Clear();
+            txtNumero2.Clear();
+            txtNumero3.Clear();
         }
     }
 }
