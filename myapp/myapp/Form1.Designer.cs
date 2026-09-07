b3362c7a3b24222e2de6dd2c6f338d3d5ac8bf9f
Galen Robles <greciapicazoxd@gmail.com>
2026-09-07 15:53:52 -0600

    temporizador

--- myapp/myapp/Form1.Designer.cs
+++ myapp/myapp/Form1.Designer.cs
@@ -32,11 +32,12 @@
             this.tmrReloj = new System.Windows.Forms.Timer(this.components);
             this.lblRelog = new System.Windows.Forms.Label();
             this.lblfecha = new System.Windows.Forms.Label();
+            this.lblEjecucion = new System.Windows.Forms.Label();
+            this.btnEncender = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // tmrReloj
             // 
-            this.tmrReloj.Enabled = true;
             this.tmrReloj.Interval = 1000;
             this.tmrReloj.Tick += new System.EventHandler(this.tmrReloj_Tick);
             // 
@@ -60,11 +61,32 @@
             this.lblfecha.TabIndex = 2;
             this.lblfecha.Text = "0";
             // 
+            // lblEjecucion
+            // 
+            this.lblEjecucion.AutoSize = true;
+            this.lblEjecucion.Location = new System.Drawing.Point(97, 349);
+            this.lblEjecucion.Name = "lblEjecucion";
+            this.lblEjecucion.Size = new System.Drawing.Size(156, 20);
+            this.lblEjecucion.TabIndex = 3;
+            this.lblEjecucion.Text = "Tiempo en Ejecucion";
+            // 
+            // btnEncender
+            // 
+            this.btnEncender.Location = new System.Drawing.Point(685, 385);
+            this.btnEncender.Name = "btnEncender";
+            this.btnEncender.Size = new System.Drawing.Size(103, 39);
+            this.btnEncender.TabIndex = 4;
+            this.btnEncender.Text = "Encender";
+            this.btnEncender.UseVisualStyleBackColor = true;
+            this.btnEncender.Click += new System.EventHandler(this.btnEncender_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnEncender);
+            this.Controls.Add(this.lblEjecucion);
             this.Controls.Add(this.lblfecha);
             this.Controls.Add(this.lblRelog);
             this.Name = "Form1";
@@ -80,6 +102,8 @@
         private System.Windows.Forms.Timer tmrReloj;
         private System.Windows.Forms.Label lblRelog;
         private System.Windows.Forms.Label lblfecha;
+        private System.Windows.Forms.Label lblEjecucion;
+        private System.Windows.Forms.Button btnEncender;
     }
 }
 
--- myapp/myapp/Form1.cs
+++ myapp/myapp/Form1.cs
@@ -12,6 +12,9 @@ namespace myapp
 {
     public partial class Form1 : Form
     {
+        int contador = 0;
+        bool bandera = false;
+        int minutos = 0;
         public Form1()
         {
             InitializeComponent();
@@ -24,9 +27,34 @@ namespace myapp
 
         private void tmrReloj_Tick(object sender, EventArgs e)
         {
-            DateTime tiempo=DateTime.Now;
+            DateTime tiempo = DateTime.Now;
             lblRelog.Text = tiempo.ToString("HH:mm:ss");
             lblfecha.Text = tiempo.ToString("MM-dd-yyyy");
+            contador++;
+            if (contador == 60)
+            {
+                minutos++;
+                contador = 0;
+            }
+            lblEjecucion.Text = "Tiempo de ejecucion " + minutos.ToString()+"minutos llevas en la ventana";
+            
+        }
+
+        private void btnEncender_Click(object sender, EventArgs e)
+        {
+            if (bandera == false)
+            {
+                bandera= true;
+                tmrReloj.Enabled = true;
+                btnEncender.Text = "Apagar";
+                  
+            }
+            else
+            {
+                bandera= false;
+                tmrReloj.Enabled = false;
+                btnEncender.Text = "Encender";
+            }
         }
     }
 }
